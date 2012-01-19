c8c9b153aecbc3f77bd89945e44f350f84272e5b
Sean Ryan <str_ie@yahoo.co.uk>
2012-01-19 22:34:26 +0000

    close explorer windows - works on Win7

--- test__closeWindows.bat
+++ test__closeWindows.bat
@@ -1,12 +1,19 @@
-REM open some Exploder windows:
+REM TEST script that closes any duplicate Exploder windows:
+REM works on the titles
 
 SETLOCAL
 
 SET PATH_TO_AU3_DIR=c:\Program Files (x86)\AutoIt3
 
+REM GOTO _SKIP_WIN_OPEN
+
 explorer %USERPROFILE%\Music
 explorer %USERPROFILE%\Music
 explorer %USERPROFILE%\Music
+
+pause
+
+:_SKIP_WIN_OPEN
 
 "%PATH_TO_AU3_DIR%\AutoIt3.exe" closeWindows.au3
 
