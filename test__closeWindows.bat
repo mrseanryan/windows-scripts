6154275629759a3e0dec5d7a7ce296c03fff154a
sean <sean@localhost>
2012-01-19 21:54:17 +0000

    close explorer windows - early

---
+++ test__closeWindows.bat
@@ -0,0 +1,13 @@
+REM open some Exploder windows:
+
+SETLOCAL
+
+SET PATH_TO_AU3_DIR=c:\Program Files (x86)\AutoIt3
+
+explorer %USERPROFILE%\Music
+explorer %USERPROFILE%\Music
+explorer %USERPROFILE%\Music
+
+"%PATH_TO_AU3_DIR%\AutoIt3.exe" closeWindows.au3
+
+pause
