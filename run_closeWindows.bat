e7e36c5dd4226d0602c09ac8018adf24729b3577
Sean Ryan <str_ie@yahoo.co.uk>
2012-01-21 18:19:45 +0000

    closeWindows.au3 OK - rename

--- run_closeWindows.bat
+++ run_closeWindows.bat
@@ -6,6 +6,6 @@ SETLOCAL
 SET PATH_TO_AU3_DIR=c:\Program Files (x86)\AutoIt3
 REM SET PATH_TO_AU3_DIR=c:\Program Files\AutoIt3
 
-"%PATH_TO_AU3_DIR%\AutoIt3.exe" closeWindows.au3
+"%PATH_TO_AU3_DIR%\AutoIt3.exe" closeDuplicateWindows.au3
 
 pause
