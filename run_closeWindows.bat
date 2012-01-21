348ccc7a9f85b01bc7c725d5c74bac89fab55ee7
Sean Ryan <str_ie@yahoo.co.uk>
2012-01-21 18:17:39 +0000

    closeWindows.au3 OK

--- run_closeWindows.bat
+++ run_closeWindows.bat
@@ -4,6 +4,7 @@ REM works on the titles
 SETLOCAL
 
 SET PATH_TO_AU3_DIR=c:\Program Files (x86)\AutoIt3
+REM SET PATH_TO_AU3_DIR=c:\Program Files\AutoIt3
 
 "%PATH_TO_AU3_DIR%\AutoIt3.exe" closeWindows.au3
 
--- test__closeWindows.bat
+++ test__closeWindows.bat
@@ -4,12 +4,13 @@ REM works on the titles
 SETLOCAL
 
 SET PATH_TO_AU3_DIR=c:\Program Files (x86)\AutoIt3
+REM SET PATH_TO_AU3_DIR=c:\Program Files\AutoIt3
 
 REM GOTO _SKIP_WIN_OPEN
 
-explorer %USERPROFILE%\Music
-explorer %USERPROFILE%\Music
-explorer %USERPROFILE%\Music
+explorer %USERPROFILE%
+explorer %USERPROFILE%
+explorer %USERPROFILE%
 
 pause
 
