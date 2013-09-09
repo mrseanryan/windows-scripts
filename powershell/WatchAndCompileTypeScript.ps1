efd73c58af43368d66e6854b6e24ff86d4911240
str <str@localhost>
2013-09-09 14:58:07 +0200

    watch and compile TypeScript files

---
+++ powershell/WatchAndCompileTypeScript.ps1
@@ -0,0 +1,26 @@
+﻿#watch a directory, for changes to TypeScript files.
+#
+#when a file changes, then re-compile it.
+
+
+$watcher = New-Object System.IO.FileSystemWatcher
+#$watcher.Path = "V:\temp"
+$watcher.Path = "V:\tfs12.II_SPOC\Wells\SPOC\SPOC Development\Source\Spoc.HMI\Spoc.HMI.Web\Scripts\TS"
+$watcher.IncludeSubdirectories = $true
+$watcher.EnableRaisingEvents = $true
+
+$changed = Register-ObjectEvent $watcher "Changed" -Action {
+    
+    if ($($eventArgs.FullPath).EndsWith(".ts"))
+    {
+        $command = '"c:\Program Files (x86)\Microsoft SDKs\TypeScript\tsc.exe" "$($eventArgs.FullPath)"'
+
+        write-host '>>> Recompiling file ' $($eventArgs.FullPath)
+
+        iex "& $command"
+    }
+}
+
+write-host 'changed.Id:' $changed.Id
+
+# Unregister-Event $changed.Id
