c112e4afa566d812598e8d51ac1c66a2ab3aeece
Sean Ryan <str_ie@yahoo.co.uk>
2013-09-23 16:34:26 +0200

    adding PS script, to view processes and their window title, memory usage.

---
+++ powershell/MemoryByWindowTitle.ps1
@@ -0,0 +1,7 @@
+﻿
+ #note: IE has only 1 window name, and this is the name of the last tab / window.
+ get-process |where {$_.ProcessName -eq 'iexplore'} |format-table name,mainwindowtitle, Handles, WS, NPM, PM, VM, Id, ProcessName  –AutoSize
+
+
+ get-process |where {$_.ProcessName -eq 'iexplore'}
+
