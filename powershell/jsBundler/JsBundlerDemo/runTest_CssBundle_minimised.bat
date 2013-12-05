b1b73411b61a316285076133204b5599016831ce
Sean Ryan <str_ie@yahoo.co.uk>
2013-12-05 11:57:02 +0100

    adding support for CSS bundling

---
+++ powershell/jsBundler/JsBundlerDemo/runTest_CssBundle_minimised.bat
@@ -0,0 +1,11 @@
+@ECHO OFF
+SETLOCAL
+
+SET OUTPATH=JsBundlerDemo\Styles
+
+powershell -f JsBundler.ps1 JsBundlerDemo\Styles\Styles.css.bundle JsBundlerDemo -m
+ECHO Errorlevel = %ERRORLEVEL%
+
+echo ===
+
+dir %OUTPATH%
