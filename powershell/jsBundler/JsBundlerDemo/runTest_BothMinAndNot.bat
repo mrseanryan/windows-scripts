23bb808b6c0935d8f264cbe9993516f1eb4e4aaf
Sean Ryan <str_ie@yahoo.co.uk>
2013-12-05 17:01:42 +0100

    adding 2 test scripts for bundler

---
+++ powershell/jsBundler/JsBundlerDemo/runTest_BothMinAndNot.bat
@@ -0,0 +1,15 @@
+@ECHO OFF
+SETLOCAL
+
+SET OUTPATH=JsBundlerDemo\Scripts\FibonaciBundle.*
+
+powershell -f JsBundler.ps1 JsBundlerDemo\Scripts\FibonaciBundle.js.bundle JsBundlerDemo -b
+ECHO Errorlevel = %ERRORLEVEL%
+
+echo ===
+type JsBundlerDemo\Scripts\FibonaciBundle.js
+echo ===
+type JsBundlerDemo\Scripts\FibonaciBundle.min.js
+echo ===
+
+dir %OUTPATH%
---
+++ powershell/jsBundler/JsBundlerDemo/runTest_CssBundle_BothMinAndNot.bat
@@ -0,0 +1,15 @@
+@ECHO OFF
+SETLOCAL
+
+SET OUTPATH=JsBundlerDemo\Styles
+
+powershell -f JsBundler.ps1 JsBundlerDemo\Styles\Styles.css.bundle JsBundlerDemo -b
+ECHO Errorlevel = %ERRORLEVEL%
+
+echo ===
+type %OUTPATH%\styles.css
+echo ===
+type %OUTPATH%\styles.min.css
+echo ===
+
+dir %OUTPATH%
