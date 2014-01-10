a71185dff54df717998d8917a05dd60e537db718
str_ie <str_ie@yahoo.co.uk>
2014-01-10 19:24:45 +0100

    tidy up - moved old PS version into its own directory.

--- powershell/jsBundler/JsBundlerDemo/JsBundlerPowershell_old/runTest_CssBundle_BothMinAndNot.bat
+++ powershell/jsBundler/JsBundlerDemo/JsBundlerPowershell_old/runTest_CssBundle_BothMinAndNot.bat
@@ -1,9 +1,9 @@
 @ECHO OFF
 SETLOCAL
 
-SET OUTPATH=JsBundlerDemo\Styles
+SET OUTPATH=..\JsBundlerDemo\Styles
 
-powershell -f JsBundler.ps1 JsBundlerDemo\Styles\Styles.css.bundle JsBundlerDemo -b
+powershell -f JsBundler.ps1 ..\JsBundlerDemo\Styles\Styles.css.bundle ..\JsBundlerDemo -b
 ECHO Errorlevel = %ERRORLEVEL%
 
 echo ===
