43b5ed6e9239cff5f694594f162cf966232415bd
Sean Ryan <str_ie@yahoo.co.uk>
2016-05-13 15:21:21 +0200

    delDirWithLongName - output is now silent, unless error!

--- delDirWithLongName.bat
+++ delDirWithLongName.bat
@@ -1,12 +1,9 @@
 @ECHO OFF
-ECHO USAGE:  delDirWithLongName.bat {path to dir}
-
-ECHO .
-
-ECHO This will destroy the directory at '%1'
+ECHO This script will destroy the directory at %1
 ECHO Press CTRL + C to cancel
 PAUSE
 
+ECHO Deleting %1 ...
 SETLOCAL
 
 SET DIR_TO_DELETE=%1
@@ -17,14 +14,19 @@ IF "%DIR_TO_DELETE%" EQU "" (GOTO _ERROR_USAGE)
 IF EXIST "%EMPTY_DIR%" (rmdir /q /s "%EMPTY_DIR%")
 
 MKDIR "%EMPTY_DIR%"
-ROBOCOPY "%EMPTY_DIR%" "%DIR_TO_DELETE%" /S /MIR
+SET _VERBOSITY=/NFL /NJH /NJS /NDL /NC /NS
+REM >nul (stdout is not echoed)
+ROBOCOPY "%EMPTY_DIR%" "%DIR_TO_DELETE%" /S /MIR %_VERBOSITY% >nul
 RMDIR /q /s "%EMPTY_DIR%"
 RMDIR /q /s "%DIR_TO_DELETE%"
 
+ECHO Deleting %1 [done]
 GOTO _END
 
 :_ERROR_USAGE
-ECHO Incorrect argumente!
+ECHO Incorrect arguments!
+ECHO .
+ECHO USAGE:  _delDirWithLongName.bat {path to dir}
 GOTO _ERROR
 
 :_ERROR
@@ -33,4 +35,3 @@ _error_occurred
 REM not using exit as that can cause whole cmd session to exit
 
 :_END
-ECHO [done]
